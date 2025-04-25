671995d73f782c4261f45ae0744859628d0a5cd0
sangsahh <hammersang@gmail.com>
2025-04-25 13:57:36 +0900

    Initial commit

---
+++ src/test/java/ko/spring/start/startspring/StartspringApplicationTests.java
@@ -0,0 +1,13 @@
+package ko.spring.start.startspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
